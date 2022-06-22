793596465fc05de0e4702e088b4ebef835a61333
karimsayed3 <karim_20180428@fci.helwan.edu.eg>
2022-06-23 00:49:55 +0200

    commit

--- lib/presentation/home/main_food_page.dart
+++ lib/presentation/home/main_food_page.dart
@@ -25,7 +25,6 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 //   ],
                 // ),
                 Text('City'),
-
               ],
             ),
             Container(
